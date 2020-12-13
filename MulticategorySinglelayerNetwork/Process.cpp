573269306ceae6c8a9f18a788cac83ecf4f969f3
Batuhan UZUN <39219223+uzunb@users.noreply.github.com>
2020-12-14 00:22:06 +0300

    YPoint function edited

--- MulticategorySinglelayerNetwork/Process.cpp
+++ MulticategorySinglelayerNetwork/Process.cpp
@@ -2,5 +2,7 @@
 
 int YPoint(int x, double* w, int carpan)
 {
-	return (int)((double)(-1 * (double)carpan * w[2] - w[0] * x) / (double)(w[1]));
+	return (int)((double)((double)carpan * w[2] - x * w[0]) / (double)(w[1]));
+
 }//(-1 * w2 - w0 * x) / w1
+//(cls[i].w[2] - minX * cls[i].w[0]) / cls[i].w[1];
