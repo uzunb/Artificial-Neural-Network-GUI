f128fafcbaadaf9baa6aef4f99cd0dd126d6953a
Batuhan UZUN <39219223+uzunb@users.noreply.github.com>
2020-12-17 17:37:32 +0300

    Overloaded Ypoint functon. Edited according to multilayer network.

--- MultilayerNetwork/Process.cpp
+++ MultilayerNetwork/Process.cpp
@@ -6,3 +6,10 @@ int YPoint(int x, double* w, int carpan)
 
 }//(-1 * w2 - w0 * x) / w1
 //(cls[i].w[2] - minX * cls[i].w[0]) / cls[i].w[1];
+
+int YPoint(int x, double* w, int offset,int carpan)
+{
+	return (int)((double)((double)carpan * w[offset + 2] - x * w[offset]) / (double)(w[offset + 1]));
+
+}//(-1 * w2 - w0 * x) / w1
+//(cls[i].w[2] - minX * cls[i].w[0]) / cls[i].w[1];
--- MultilayerNetwork/Process.h
+++ MultilayerNetwork/Process.h
@@ -1,2 +1,3 @@
 
 int YPoint(int x, double* w, int = 1);
+int YPoint(int x, double* w, int offset, int carpan);
