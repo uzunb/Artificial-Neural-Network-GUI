913a8150dbd225c4ab5b247081892fd77b0597c2
Batuhan UZUN <39219223+uzunb@users.noreply.github.com>
2020-12-17 17:39:00 +0300

    It will edit according multilayer network

--- MultilayerNetwork/PerceptronLearning.cpp
+++ MultilayerNetwork/PerceptronLearning.cpp
@@ -15,39 +15,39 @@ PerceptronLearning::~PerceptronLearning()
 
 void PerceptronLearning::Train(Samples* input, double* w, int numberOfInputs, SampleClass* cls, int numberOfClasses, int &cycleCount)
 {
-    bool isUpdated;
-    int d, output;
-    double net, temp, error;
+    //bool isUpdated;
+    //int d, output;
+    //double net, temp, error;
 
-    //For each class weights
-    for (int clsNumber = 0; clsNumber < numberOfClasses; clsNumber++)
-    {
-        do
-        {
-            error = 0;
-            // until network give not error for all samples
-            for (int i = 0; i < numberOfInputs; i++)
-            {
-                //expected value
-                cls[clsNumber].classId == input[i].classId ? d = 1 : d = -1;
+    ////For each class weights
+    //for (int clsNumber = 0; clsNumber < numberOfClasses; clsNumber++)
+    //{
+    //    do
+    //    {
+    //        error = 0;
+    //        // until network give not error for all samples
+    //        for (int i = 0; i < numberOfInputs; i++)
+    //        {
+    //            //expected value
+    //            cls[clsNumber].classId == input[i].classId ? d = 1 : d = -1;
 
-                //w^t * x   
-                net = cls[clsNumber].w[0] * input[i].x[0] + cls[clsNumber].w[1] * input[i].x[1] + cls[clsNumber].w[2] * input[i].x[2];
+    //            //w^t * x   
+    //            net = cls[clsNumber].w[0] * input[i].x[0] + cls[clsNumber].w[1] * input[i].x[1] + cls[clsNumber].w[2] * input[i].x[2];
 
-                output = sgn(net);   //activation function
-                if (d != output) {
-                    
-                    //w^n+1 = w^n + c * ( d - sgn(net) ) * x
-                    temp = 0.5*this->getC() * (d - output);
-                    cls[clsNumber].w[0] = cls[clsNumber].w[0] + temp * input[i].x[0];
-                    cls[clsNumber].w[1] = cls[clsNumber].w[1] + temp * input[i].x[1];
-                    cls[clsNumber].w[2] = cls[clsNumber].w[2] + temp * input[i].x[2];
+    //            output = sgn(net);   //activation function
+    //            if (d != output) {
+    //                
+    //                //w^n+1 = w^n + c * ( d - sgn(net) ) * x
+    //                temp = 0.5*this->getC() * (d - output);
+    //                cls[clsNumber].w[0] = cls[clsNumber].w[0] + temp * input[i].x[0];
+    //                cls[clsNumber].w[1] = cls[clsNumber].w[1] + temp * input[i].x[1];
+    //                cls[clsNumber].w[2] = cls[clsNumber].w[2] + temp * input[i].x[2];
 
-                    error += 0.5 * (d - output) * (d - output);
-                }
-            }
-            ++cycleCount;
-        } while (error > 0.05);
-    }
+    //                error += 0.5 * (d - output) * (d - output);
+    //            }
+    //        }
+    //        ++cycleCount;
+    //    } while (error > 0.05);
+    //}
 }
 
