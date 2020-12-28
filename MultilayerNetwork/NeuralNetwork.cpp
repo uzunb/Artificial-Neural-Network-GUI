35ae66201877c291388686ff58011bcdeefdde76
Batuhan UZUN <39219223+uzunb@users.noreply.github.com>
2020-12-28 13:46:41 +0300

    fix bipolar sigmoid function

--- MultilayerNetwork/NeuralNetwork.cpp
+++ MultilayerNetwork/NeuralNetwork.cpp
@@ -94,21 +94,27 @@ int NeuralNetwork::sgn(int number)
 	return number >= 0 ? 1 : -1;
 }
 
-double NeuralNetwork::sigmoid(double net, double delta = 1) 
+double NeuralNetwork::sigmoid(double net, double delta = 0.5	) 
 {
-	double temp1 = exp(-delta * net);
-
-	double returnValue = (2 / (1 + temp1)) - 1;
-	//return std::isfinite(returnValue) ? returnValue : this->sgn(returnValue);
-	if (isfinite(returnValue))
+	double returnValue = (2 / (1 + exp(-delta * net)) - 1);
+	if (isfinite(returnValue)) {
 		return returnValue;
+	}
 	else
-		return returnValue >= 0 ? 1 : -1;
+		return	returnValue >= 0 ? 1 : -1;
 }
 
-double NeuralNetwork::derivatedSigmoid(double output, int desiredValue)
+double NeuralNetwork::derivatedSigmoid(double output, int desiredValue = 1)
 {
-	return 0.5 * (1 - output*output);
+	double returnValue = 0.5 * (1 - output * output);
+
+	if (isfinite(returnValue)) {
+		if (returnValue >= 1)  returnValue = 1;
+		else if (returnValue <= -1) returnValue = -1;
+		return returnValue;
+	}
+	else
+		return	returnValue >= 0 ? 1 : -1;
 }
 
 double NeuralNetwork::logistic(double net)
@@ -159,10 +165,9 @@ void NeuralNetwork::MatrixMultiplication(double* m1, int col1, int row1, double*
 }
 
 
-void NeuralNetwork::MatrixMultiplication(double* m1, int col1, int row1, double* m2, int col2, int row2, double* m3, char* activationFunc)
+void NeuralNetwork::MatrixMultiplication(double* m1, int col1, int row1, double* m2, int col2, int row2, double* m3, char* activationFunc, float lambda)
 {
-	if (col1 != row2)
-		return;
+	if (col1 != row2)		exit(1);
 
 	double sum = 0;
 	for (int i = 0; i < row1; i++) {
@@ -172,11 +177,15 @@ void NeuralNetwork::MatrixMultiplication(double* m1, int col1, int row1, double*
 				sum += m1[i * col1 + k] * m2[k * col2 + j];
 			}
 			if (!strcmp(activationFunc, "sigmoid"))
-				m3[i * col2 + j] = sigmoid(sum);
+				m3[i * col2 + j] = sigmoid(sum, lambda);
+			else if (!strcmp(activationFunc, "dsigmoid"))
+				m3[i * col2 + j] = derivatedSigmoid(sum);
+			else if (!strcmp(activationFunc, "relu"))
+				m3[i * col2 + j] = reLU(sum);
 			else if (!strcmp(activationFunc, "sign"))
 				m3[i * col2 + j] = sgn(sum);
 			else
-				return;
+				exit(1);
 
 		}
 	}
--- MultilayerNetwork/NeuralNetwork.h
+++ MultilayerNetwork/NeuralNetwork.h
@@ -11,12 +11,13 @@ protected:
 	double c; //learning constant
 
 public:
-	double* v;
-	int* v_color;
-	double* y;
-	double* w;
-	double* o;
-
+	double* v;		//hiddenNeuronNumber * (2 + 1)
+	int* v_color;	//hiddenNeuronNumber * 3(RGB)
+	double* y;		//(hiddenNeuronNumber + 1) * 1
+	double* w;		//(hiddenNeuronNumber + 1) * classNumber
+	double* o;		//classNumber * 1
+	int* d;			//classNumber * 1
+		
 	void setLayerCount(int count);
 	void setNeuronCount(int count);
 	void setInput(int* input);
@@ -43,7 +44,7 @@ public:
 	double softmax(double net);
 
 	void MatrixMultiplication(double* m1, int col1, int row1, double* m2, int col2, int row2, double* m3);
-	void MatrixMultiplication(double* m1, int col1, int row1, double* m2, int col2, int row2, double* m3, char* activationFunc);
+	void MatrixMultiplication(double* m1, int col1, int row1, double* m2, int col2, int row2, double* m3, char* activationFunc, float lambda);
 	void MatrixMultiplication(double* matx, int col, int row, double scalerNumber, double* matx2);
 
 };
